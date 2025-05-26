492d702e30a241133dc5bf5bfa3a138c99c3df13
Gustavo Martins Pereira <gustavo.martins.pereira.main@gmail.com>
2025-05-26 15:11:55 -0300

    ADD - Input Controller

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,23 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late final TextEditingController _email = TextEditingController();
+  late final TextEditingController _password = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _email.dispose();
+    _password.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,20 +29,31 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
-      home: const HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Register"),
-        backgroundColor: Theme.of(context).colorScheme.primary,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Register"),
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        ),
+        body: Column(
+          children: [
+            TextField(
+              controller: _email,
+              decoration: InputDecoration(
+                hintText: "Enter your email here"
+              ),
+            ),
+            TextField(
+              controller: _password,
+              decoration: InputDecoration(
+                hintText: "Enter your password here"
+              ),
+            ),
+            TextButton(
+              onPressed: () async {},
+              child: Text("Register"),
+            ),
+          ],
+        ),
       ),
     );
   }
